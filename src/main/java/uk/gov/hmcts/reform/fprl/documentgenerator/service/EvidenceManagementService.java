59b807288e1d41250e3ae07753d492ee2abbb6ad
Patryk Jar <patryk.jar@hmcts.net>
2021-10-14 16:07:37 +0200

    cleaning up after EvidenceManagementService

--- src/main/java/uk/gov/hmcts/reform/fprl/documentgenerator/service/EvidenceManagementService.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.fprl.documentgenerator.service;
-
-import uk.gov.hmcts.reform.fprl.documentgenerator.domain.response.FileUploadResponse;
-
-public interface EvidenceManagementService {
-    FileUploadResponse storeDocumentAndGetInfo(byte[] document, String authorizationToken, String fileName);
-}
