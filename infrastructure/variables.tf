8e4578d84935146c1ffa93c22d4ac5ac5856770e
shashis97 <shashikumar.sannakariyappa@cognizant.com>
2021-10-19 18:19:13 +0100

    removing unwanted fields

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,73 +1,17 @@
-variable "reform_service_name" {
-  default = "dgs"
-}
-
-variable "reform_team" {
-  default = "fprl"
-}
-
-variable "capacity" {
-    default = "1"
-}
-
-variable "instance_size" {
-    default = "I2"
-}
-
-variable "component" {}
-
-variable "env" {}
-
 variable "product" {}
 
-variable "raw_product" {
-  default = "fprl"
-}
-
-variable "tenant_id" {}
-
-variable "jenkins_AAD_objectId" {
-  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
-}
-
-variable "appinsights_instrumentation_key" {
-    description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-    default = ""
-}
-
-variable "idam_s2s_url_prefix" {
-  default = "rpe-service-auth-provider"
-}
-
-variable "auth_provider_service_client_microservice" {
-  default = "fprl_dgs_api"
-}
-
-variable "auth_provider_service_client_tokentimetoliveinseconds" {
-  default = "900"
-}
-
-variable "subscription" {}
+variable "component" {}
 
 variable "location" {
   default = "UK South"
 }
 
-variable "ilbIp" {}
+variable "env" {}
 
-variable "vault_env" {
-    default = "preprod"
-}
+variable "subscription" {}
+
+variable "deployment_namespace" {}
 
 variable "common_tags" {
-    type = map(string)
+  type = "map"
 }
-
-variable "health_check_ttl" {
-    default = "5000"
-}
-
-variable "feature_resp_solicitor_details" {
-  default = "false"
-}
-
